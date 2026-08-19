f27fce6767909f47d21fd6e9139a0a2567fc0b29
Tamires Ferreira <114240027+tamiresf@users.noreply.github.com>
2026-08-19 14:23:57 -0300

    Update gemba_app.py

--- gemba_app.py
+++ gemba_app.py
@@ -26,7 +26,7 @@ WEBHOOK_RESOLVER = st.secrets.get(
 # NOVA URL DO SHAREPOINT AJUSTADA COM PARÂMETRO DE DOWNLOAD
 EXCEL_READ_URL = st.secrets.get(
     "EXCEL_READ_URL", 
-    "https://mustad365.sharepoint.com/:x:/s/LATAM/MBL/IQAPSTMSVrHsQLv4AQ3Yzxq8AQaT4uj9nirde2JN9qh2glE?e=1cc8dp"
+    "https://mustad365.sharepoint.com/:x:/s/LATAM/MBL/IQAPSTMSVrHsQLv4AQ3Yzxq8AQaT4uj9nirde2JN9qh2glE?e=JrIAOY"
 )
 
 CATEGORIAS = {
