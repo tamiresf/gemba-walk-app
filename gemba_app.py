580e48db91735f1dff6c527265687898f3415e6d
Tamires Ferreira <114240027+tamiresf@users.noreply.github.com>
2026-08-25 11:18:43 -0300

    Update gemba_app.py

--- gemba_app.py
+++ gemba_app.py
@@ -210,7 +210,7 @@ with aba1:
 
         with col_prazo:
             # Destaque visual chamativo para o campo de Prazo
-            st.markdown("🚨 **Atenção **")
+            st.markdown("🚨 **Atenção**")
             prazo = st.date_input(
                 "📅 Prazo para Solução*",
                 value=date.today(),
